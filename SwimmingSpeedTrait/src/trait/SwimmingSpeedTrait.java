5eda0593296c23a00e1c62735cbdd5e0a95df5fd
Tobias Welther <tobiyasw@gmail.com>
2015-01-18 14:15:07 +0100

    Adding Basics for Swimming Speed Trait. ONLY WITH MC 1.8 and higher.

--- SwimmingSpeedTrait/src/trait/SwimmingSpeedTrait.java
+++ SwimmingSpeedTrait/src/trait/SwimmingSpeedTrait.java
@@ -15,48 +15,66 @@
  ******************************************************************************/
 package trait;
 
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Event;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
-import org.bukkit.event.player.PlayerLoginEvent.Result;
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.ItemStack;
 
-import de.tobiyas.racesandclasses.datacontainer.traitholdercontainer.AbstractTraitHolder;
+import de.tobiyas.racesandclasses.datacontainer.player.RaCPlayer;
+import de.tobiyas.racesandclasses.datacontainer.player.RaCPlayerManager;
 import de.tobiyas.racesandclasses.eventprocessing.eventresolvage.EventWrapper;
 import de.tobiyas.racesandclasses.eventprocessing.events.holderevent.HolderSelectedEvent;
-import de.tobiyas.racesandclasses.traitcontainer.interfaces.AbstractBasicTrait;
-import de.tobiyas.racesandclasses.traitcontainer.interfaces.TraitResults;
+import de.tobiyas.racesandclasses.traitcontainer.interfaces.annotations.configuration.RemoveSuperConfigField;
 import de.tobiyas.racesandclasses.traitcontainer.interfaces.annotations.configuration.TraitConfigurationField;
 import de.tobiyas.racesandclasses.traitcontainer.interfaces.annotations.configuration.TraitConfigurationNeeded;
 import de.tobiyas.racesandclasses.traitcontainer.interfaces.annotations.configuration.TraitEventsUsed;
 import de.tobiyas.racesandclasses.traitcontainer.interfaces.annotations.configuration.TraitInfos;
 import de.tobiyas.racesandclasses.traitcontainer.interfaces.markerinterfaces.Trait;
+import de.tobiyas.racesandclasses.traitcontainer.traits.pattern.TickEverySecondsTrait;
 import de.tobiyas.racesandclasses.util.traitutil.TraitConfiguration;
 import de.tobiyas.racesandclasses.util.traitutil.TraitConfigurationFailedException;
 
-public class SwimmingSpeedTrait extends AbstractBasicTrait {
+public class SwimmingSpeedTrait extends TickEverySecondsTrait {
 
-	private double value;
+	/**
+	 * The Level to set (1-3).
+	 */
+	private int level = 3;
+	
+	/**
+	 * The Players with opened Inventories.
+	 */
+	private Set<UUID> playerWithOpenedInventories = new HashSet<UUID>();
+	
+	/**
+	 * The Players with active Enchants.
+	 */
+	private Set<UUID> playerWithActiveEnchants = new HashSet<UUID>();
 	
 	
 	@TraitEventsUsed(registerdClasses = {PlayerLoginEvent.class, HolderSelectedEvent.class})
 	@Override
 	public void generalInit() {
+		super.generalInit();
 	}
 
 	@Override
 	public String getName() {
 		return "SwimmingSpeedTrait";
-	}
-
-	@Override
-	protected String getPrettyConfigIntern(){
-		return "speed: " + value;
 	}
 
 
@@ -69,93 +87,162 @@ public class SwimmingSpeedTrait extends AbstractBasicTrait {
 	@Override
 	public boolean isBetterThan(Trait trait) {
 		if(trait instanceof SwimmingSpeedTrait){
-			return value > ((SwimmingSpeedTrait)trait).value;
+			return level > ((SwimmingSpeedTrait)trait).level;
 		}
 		return false;
 	}
 
-	@Override
-	public TraitResults trigger(EventWrapper eventWrapper) {   Event event = eventWrapper.getEvent();
-		if(event instanceof PlayerLoginEvent){
-			PlayerLoginEvent loginEvent = (PlayerLoginEvent) event;
-			final Player player = loginEvent.getPlayer();
-			
-			
-			if(loginEvent.getResult() == Result.ALLOWED){
-				return new TraitResults( setPlayerSpeed(player));
-			}
-		}
-		
-
-		if(event instanceof HolderSelectedEvent){
-			HolderSelectedEvent HolderSelectedEvent = (HolderSelectedEvent) event;
-			final Player player = HolderSelectedEvent.getPlayer();			
-			return new TraitResults(setPlayerSpeed(player));
-		}
-		
-		return TraitResults.False();
-	}
-	
-	
-	/**
-	 * Sets the Speed of the player modified to the current Trait.
-	 * 
-	 * @param player to set
-	 * 
-	 * @return true if worked, false otherwise
-	 */
-	private boolean setPlayerSpeed(final Player player){
-		final float convertedValue = (float) value;
-		if(value < 0 || value > 1) return false;
-		
-		//Schedule this to the next tics since it will be overwritten...
-		Bukkit.getServer().getScheduler().scheduleSyncDelayedTask((JavaPlugin)plugin, new Runnable() {
-			@Override
-			public void run() {
-				player.setWalkSpeed(convertedValue);
-			}
-		}, 2);
-
-		return true;
-	}
 
 	
 	@TraitConfigurationNeeded(fields = {
-			@TraitConfigurationField(fieldName = "value", classToExpect = Double.class)
-		})
+			@TraitConfigurationField(fieldName = "level", classToExpect = Integer.class, optional = true)
+		}, removedFields = {
+			@RemoveSuperConfigField(name = "seconds")
+	})
 	@Override
 	public void setConfiguration(TraitConfiguration configMap) throws TraitConfigurationFailedException {
+		configMap.put("seconds", 1);
 		super.setConfiguration(configMap);
 		
-		this.value = configMap.getAsDouble("value");
+		if(configMap.containsKey("level")){
+			this.level = configMap.getAsInt("level");
+		}
 	}
 
 	
 	public static List<String> getHelpForTrait(){
 		List<String> helpList = new LinkedList<String>();
-		helpList.add(ChatColor.YELLOW + "The trait changes (increases or decreases) the movement speed of a Player.");
+		helpList.add(ChatColor.YELLOW + "The trait changes (increases or decreases) the movement speed of a Player underwater.");
 		return helpList;
 	}
+	
 
 	@Override
 	public boolean canBeTriggered(EventWrapper wrapper) {
-		Event event = wrapper.getEvent();
-		if(event instanceof PlayerLoginEvent){
-			PlayerLoginEvent loginEvent = (PlayerLoginEvent) event;
-			
-			if(loginEvent.getResult() == Result.ALLOWED){
-				return true;
-			}
-		}
-
-		if(event instanceof HolderSelectedEvent){
-			HolderSelectedEvent HolderSelectedEvent = (HolderSelectedEvent) event;
-			for(AbstractTraitHolder holder : getTraitHolders()){
-				if(HolderSelectedEvent.getHolderToSelect() == holder) return true;
-			}
-			return false;
+		boolean canBeTriggered = super.canBeTriggered(wrapper);
+		if(!canBeTriggered){
+			removeEnchant(wrapper.getPlayer());
+			playerWithActiveEnchants.remove(wrapper.getPlayer().getUniqueId());
+		}else{
+			playerWithActiveEnchants.add(wrapper.getPlayer().getUniqueId());
 		}
 		
-		return false;
+		return true;
+	}
+
+	/**
+	 * Removes an Enchantment from the Player.
+	 * 
+	 * @param racPlayer to use.
+	 */
+	private void removeEnchant(RaCPlayer racPlayer) {
+		if(!racPlayer.isOnline()) return;
+		Player player = racPlayer.getPlayer();
+		
+		ItemStack boots = player.getInventory().getBoots();
+		if(boots == null || boots.getType() == Material.AIR) return;
+		
+		boots.removeEnchantment(Enchantment.DEPTH_STRIDER);
+		player.getInventory().setBoots(boots);
+	}
+	
+
+	/**
+	 * Adds an Enchantment to the Player.
+	 * 
+	 * @param racPlayer to use.
+	 */
+	private void addEnchant(RaCPlayer racPlayer) {
+		if(!racPlayer.isOnline()) return;
+		Player player = racPlayer.getPlayer();
+		
+		ItemStack boots = player.getInventory().getBoots();
+		if(boots == null || boots.getType() == Material.AIR) boots = new ItemStack(Material.LEATHER_BOOTS);
+		
+		//if already has enchant. Remove him from the Naughty list.
+		if(boots.getEnchantmentLevel(Enchantment.DEPTH_STRIDER) > 0){
+			playerWithActiveEnchants.remove(racPlayer.getUniqueId());
+			return;
+		}
+		
+		boots.addUnsafeEnchantment(Enchantment.DEPTH_STRIDER, level);
+		player.getInventory().setBoots(boots);
+	}
+	
+
+	@Override
+	protected String getPrettyConfigIntern() {
+		return "Swims " + level + "x faster underwater";
+	}
+
+	@Override
+	protected boolean tickDoneForPlayer(RaCPlayer player) {
+		if(!player.isOnline()) return false;
+		
+		Location playerLocation = player.getLocation();
+		Material standingIn = playerLocation.getBlock().getType();
+		
+		if(playerWithOpenedInventories.contains(player.getUniqueId())) return false;
+		
+		boolean alreadyOnList = playerWithActiveEnchants.contains(player.getUniqueId());
+		boolean isLiquid = false;
+		if(standingIn == Material.WATER || standingIn == Material.STATIONARY_WATER
+				|| standingIn == Material.LAVA || standingIn == Material.STATIONARY_LAVA){
+			isLiquid = true;
+		}
+		
+		if(alreadyOnList && !isLiquid){
+			playerWithActiveEnchants.remove(player.getUniqueId());
+			removeEnchant(player);
+			return true;
+		}
+		
+		if(!alreadyOnList && isLiquid){
+			playerWithActiveEnchants.add(player.getUniqueId());
+			addEnchant(player);
+			return true;
+		}
+		
+		return true;
+	}
+	
+	
+	@EventHandler
+	public void playerOpenInventory(InventoryOpenEvent event){
+		HumanEntity player = event.getPlayer();
+		if(!(player instanceof Player)) return;
+		
+		UUID id = player.getUniqueId();
+		RaCPlayer racPlayer = RaCPlayerManager.get().getPlayer(id);
+		if(racPlayer == null) return;
+		
+		if(!racPlayer.getTraits().contains(this)) return;
+		this.playerWithOpenedInventories.add(id);
+		
+		if(playerWithActiveEnchants.contains(id)) removeEnchant(racPlayer);
+	}
+	
+	@EventHandler
+	public void playerCloseInventory(InventoryCloseEvent event){
+		HumanEntity player = event.getPlayer();
+		if(!(player instanceof Player)) return;
+		
+		UUID id = player.getUniqueId();
+		RaCPlayer racPlayer = RaCPlayerManager.get().getPlayer(id);
+		if(racPlayer == null) return;
+		
+		this.playerWithOpenedInventories.remove(id);
+	}
+	
+	@EventHandler
+	public void playerLeavesServer(PlayerQuitEvent event){
+		RaCPlayer racPlayer = RaCPlayerManager.get().getPlayer(event.getPlayer());
+		if(playerWithActiveEnchants.contains(racPlayer.getUniqueId())) removeEnchant(racPlayer);
+	}
+
+	
+	@Override
+	protected String getPrettyConfigurationPre() {
+		return "";
 	}
 }
